2c51f8c46d089e3a2ee390a165758de588800687
RAED <rrrr53@hotmail.com>
2022-06-29 11:01:03 +0300

    Update VirtualKeyBoard.py

--- usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/VirtualKeyBoard.py
+++ usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/VirtualKeyBoard.py
@@ -64,8 +64,8 @@ else:
 vkLayoutDir = resolveFilename(SCOPE_PLUGINS, "SystemPlugins/NewVirtualKeyBoard/skins/kle/")
 
 # external kle layout files
-#ServerUrl = 'http://tunisia-dreambox.info/TSplugins/NewVirtualKeyBoard/kle/'
-ServerUrl = 'https://github.com/fairbird/NewVirtualKeyBoard/tree/main/kle/'
+ServerUrl = 'http://tunisia-dreambox.info/TSplugins/NewVirtualKeyBoard/kle/'
+#ServerUrl = 'https://github.com/fairbird/NewVirtualKeyBoard/tree/main/kle/'
 
 # keyboardlayout website
 # http://kbdlayout.info/
