b86b59ba471b323a9a84eb19d4a4489241b1d624
MOHAMED_OS <47327706+MOHAMED19OS@users.noreply.github.com>
2022-07-16 22:42:24 +0100

    Fix Downlaod URL subtitles for SubsSupport (#2)
    Co-authored-by: MOHAMED19OS <mohamed_os@gmail.com>

--- installer.sh
+++ installer.sh
@@ -3,26 +3,26 @@
 ###########
 version=11.9
 # remove old version
-cp -r /usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/skins/kle /tmp/ > /dev/null 2>&1
-rm -rf /usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyboard > /dev/null 2>&1
+cp -r /usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/skins/kle /tmp/ >/dev/null 2>&1
+rm -rf /usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyboard >/dev/null 2>&1
 #rm -f /usr/lib/enigma2/python/Screens/NewVirtualKeyBoard.py > /dev/null 2>&1
 #rm -f /usr/lib/enigma2/python/Screens/NewVirtualKeyBoard.pyo > /dev/null 2>&1
 #rm -f /usr/lib/enigma2/python/Screens/NewVirtualKeyBoard.pyc > /dev/null 2>&1
 # Download and install plugin
 echo " ** Download and install NewVirtualKeyBoard ** "
-cd /tmp 
+cd /tmp
 set -e
-rm -rf *main* > /dev/null 2>&1
-rm -rf *NewVirtualKeyBoard* > /dev/null 2>&1
+rm -rf *main* >/dev/null 2>&1
+rm -rf *NewVirtualKeyBoard* >/dev/null 2>&1
 wget -q "--no-check-certificate" https://github.com/fairbird/NewVirtualKeyBoard/archive/refs/heads/main.tar.gz
 tar -xzf main.tar.gz
 cp -r NewVirtualKeyBoard-main/usr /
 if [ -f '/tmp/kle' ]; then
 	cp -f /tmp/kle/* /usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/skins/kle
 fi
-rm -rf /tmp/kle > /dev/null 2>&1
-rm -rf *NewVirtualKeyBoard* > /dev/null 2>&1
-rm -rf *main* > /dev/null 2>&1
+rm -rf /tmp/kle >/dev/null 2>&1
+rm -rf *NewVirtualKeyBoard* >/dev/null 2>&1
+rm -rf *main* >/dev/null 2>&1
 echo
 echo
 set +e
@@ -35,9 +35,9 @@ if [ ! -d '/usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard' ]; 
 fi
 
 if [ -d /usr/lib/enigma2/python/Plugins/Extensions/SubsSupport ]; then
-	wget "http://tunisia-dreambox.info/TSplugins/NewVirtualKeyBoard/subtitles.py"
-	rm -f /usr/lib/enigma2/python/Plugins/Extensions/SubsSupport/subtitles.py > /dev/null 2>&1
-	mv subtitles.py /usr/lib/enigma2/python/Plugins/Extensions/SubsSupport  > /dev/null 2>&1
+	wget "https://raw.githubusercontent.com/fairbird/NewVirtualKeyBoard/main/subtitles.py"
+	rm -f /usr/lib/enigma2/python/Plugins/Extensions/SubsSupport/subtitles.py >/dev/null 2>&1
+	mv subtitles.py /usr/lib/enigma2/python/Plugins/Extensions/SubsSupport >/dev/null 2>&1
 fi
 
 sync
@@ -45,7 +45,7 @@ echo
 echo
 echo "###########################################################################"
 echo "#                 NewVirtualKeyBoard INSTALLED SUCCESSFULLY               #"
-echo "#                       mfaraj57 & RAED (fairbird)                        #"              
+echo "#                       mfaraj57 & RAED (fairbird)                        #"
 echo "#                               support                                   #"
 echo "#                         ttps://www.tunisia-sat.com                      #"
 echo "#  restart enigma2 and select New virtual keyboard setup from menu-system #"
