25881527dda377a05c5ce9def9d9aa15be7e6d83
RAED <rrrr53@hotmail.com>
2022-10-05 16:24:56 +0300

    Update

--- subsinstaller.sh
+++ subsinstaller.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-##setup command=wget http://tunisia-dreambox.info/TSplugins/NewVirtualKeyBoard/subsinstaller.sh -O - | /bin/sh
+##setup command=wget https://raw.githubusercontent.com/fairbird/NewVirtualKeyBoard/main/subsinstaller.sh -O - | /bin/sh
 
 if [ ! -d /usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard ]; then
 	echo
@@ -7,12 +7,12 @@ if [ ! -d /usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard ]; th
 	echo "*** NewVirtualKeyBoard No found ... Will be install first ***"
 	echo
 	echo
-	wget http://tunisia-dreambox.info/TSplugins/NewVirtualKeyBoard/installer.sh -O - | /bin/sh
+	wget https://raw.githubusercontent.com/fairbird/NewVirtualKeyBoard/main/installer.sh -O - | /bin/sh
 fi
 
 cd /tmp 
 if [ -d /usr/lib/enigma2/python/Plugins/Extensions/SubsSupport ]; then
-	wget "http://tunisia-dreambox.info/TSplugins/NewVirtualKeyBoard/subtitles.py"
+	wget "https://raw.githubusercontent.com/fairbird/NewVirtualKeyBoard/main/subtitles.py"
 	rm -f /usr/lib/enigma2/python/Plugins/Extensions/SubsSupport/subtitles.py > /dev/null 2>&1
 	mv subtitles.py /usr/lib/enigma2/python/Plugins/Extensions/SubsSupport  > /dev/null 2>&1
 fi
