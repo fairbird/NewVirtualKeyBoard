f901f4225dca2421913b6ebd4ecaa34a86d036f6
RAED <rrrr53@hotmail.com>
2023-04-21 18:17:33 +0300

    [compat] Use urlopen in thread to enforce a timeout. Depend on https://github.com/Taapat/enigma2-plugin-youtube/commit/2bcb10008f1c3158e2cac12f33f079cb5b699492

--- usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/compat.py
+++ usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/compat.py
@@ -16,7 +16,7 @@ if version_info[0] == 2:
 
 	from urllib import urlencode as compat_urlencode
 	from urllib import quote as compat_quote
-	from urllib2 import urlopen as compat_urlopen
+	from urllib2 import urlopen
 	from urllib2 import Request as compat_Request
 	from urllib2 import HTTPError as compat_HTTPError
 	from urllib2 import URLError as compat_URLError
@@ -30,7 +30,7 @@ else:
 
 	from urllib.parse import urlencode as compat_urlencode
 	from urllib.parse import quote as compat_quote
-	from urllib.request import urlopen as compat_urlopen
+	from urllib.request import urlopen
 	from urllib.request import Request as compat_Request
 	from urllib.error import HTTPError as compat_HTTPError
 	from urllib.error import URLError as compat_URLError
@@ -39,3 +39,27 @@ else:
 	from urllib.parse import parse_qs as compat_parse_qs
 	from urllib.parse import urlunparse as compat_urlunparse
 	from http.client import HTTPException as compat_HTTPException
+
+def compat_urlopen(url, timeout=5):
+	"""
+	Urlopen in thread to enforce a timeout on the function call.
+	Timeout in urlopen only affects how long Python waits before
+	an exception is raised if the server has not issued a response.
+	It does not enforce a time limit on the entire function call.
+	"""
+	compat_urlopen.response = None
+	compat_urlopen.error = None
+
+	def open_url(url, timeout):
+		try:
+			compat_urlopen.response = urlopen(url, timeout=timeout)
+		except Exception as e:
+			compat_urlopen.error = e
+
+	t = Thread(target=open_url, args=(url, timeout))
+	t.setDaemon(True)
+	t.start()
+	t.join(timeout + 1)
+	if compat_urlopen.error:
+		raise compat_urlopen.error
+	return compat_urlopen.response
