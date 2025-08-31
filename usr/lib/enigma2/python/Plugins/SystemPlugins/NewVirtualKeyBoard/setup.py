badc0602aed5731eb97c65c58cd283f025707a70
fairbird <rrrr53@hotmail.com>
2025-08-31 14:51:18 +0300

    update

--- usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/setup.py
+++ usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/setup.py
@@ -20,19 +20,32 @@ from Plugins.SystemPlugins.NewVirtualKeyBoard.tools import *
 VER = getversioninfo()
 
 config.NewVirtualKeyBoard = ConfigSubsection()
+############# language
+config.NewVirtualKeyBoard.lang = ConfigSelection(default="EN", choices = [
+	("EN", "English"),
+	("AR", "عربي"),
+	])
+
+if config.NewVirtualKeyBoard.lang.value == "EN":
+	from Plugins.SystemPlugins.NewVirtualKeyBoard.language.en import *
+elif config.NewVirtualKeyBoard.lang.value == "AR":
+	from Plugins.SystemPlugins.NewVirtualKeyBoard.language.ar import *
+else:
+	from Plugins.SystemPlugins.NewVirtualKeyBoard.language.en import *
+#############
 config.NewVirtualKeyBoard.keys_layout = ConfigText(default='', fixed_size=False)
-config.NewVirtualKeyBoard.lastsearchText = ConfigText(default='Enter search word', fixed_size=False)
+config.NewVirtualKeyBoard.lastsearchText = ConfigText(default='%s' % title1, fixed_size=False)
 config.NewVirtualKeyBoard.firsttime = ConfigYesNo(default=True)
-config.NewVirtualKeyBoard.textinput = ConfigSelection(default='VirtualKeyBoard', choices=[('VirtualKeyBoard', _('Image virtual keyboard')), ('NewVirtualKeyBoard', _('New Virtual Keyboard'))])
+config.NewVirtualKeyBoard.textinput = ConfigSelection(default='VirtualKeyBoard', choices=[('VirtualKeyBoard', _('%s' % title2)), ('NewVirtualKeyBoard', _('%s' % title3))])
 config.NewVirtualKeyBoard.showinplugins = ConfigYesNo(default = True)
 config.NewVirtualKeyBoard.showsuggestion = ConfigYesNo(default=True)
 config.NewVirtualKeyBoard.fontssize = ConfigInteger(default = 0, limits = (0, 30))
 config.NewVirtualKeyBoard.updateonline = ConfigYesNo(default=True)
-
 try:
         FONTSSIZE = config.NewVirtualKeyBoard.fontssize.value
 except:
         FONTSSIZE = 0
+
 
 class nvKeyboardSetup(ConfigListScreen, Screen):
 
@@ -46,13 +59,13 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
 				<widget name="config" position="30,55" size="1020,298" itemHeight="45" font="Regular;30" secondfont="Regular;28" scrollbarMode="showOnDemand" transparent="1" zPosition="2" />
 
 				<ePixmap position="30,360" size="38,38" pixmap="~/images/key_red.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="78,360" zPosition="4" size="300,38" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Cancel" />
+				<widget position="78,360" zPosition="4" size="300,38" halign="left" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_red" />
 
 				<ePixmap position="330,360" size="38,38" pixmap="~/images/key_green.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="378,360" zPosition="4" size="300,38" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Save" />
+				<widget position="378,360" zPosition="4" size="300,38" halign="left" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_green" />
 
 				<ePixmap position="630,360" size="38,38" pixmap="~/images/key_yellow.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="678,360" zPosition="4" size="420,38" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Virtual keyboard" />
+				<widget position="678,360" zPosition="4" size="420,38" halign="left" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_yellow" />
 				</screen>""" % VER
         	else:
         		self.skin = """
@@ -62,13 +75,13 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
 				<widget name="config" position="20,60" size="680,182" itemHeight="30" font="Regular;20" scrollbarMode="showOnDemand" transparent="1" zPosition="2" />
 
 				<ePixmap position="20,250" size="25,25" pixmap="~/images/key_red_sd.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="52,250" zPosition="4" size="200,25" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Cancel" />
+				<widget position="52,250" zPosition="4" size="200,25" halign="left" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_red" />
 
 				<ePixmap position="220,250" size="25,25" pixmap="~/images/key_green_sd.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="252,250" zPosition="4" size="200,25" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Save" />
+				<widget position="252,250" zPosition="4" size="200,25" halign="left" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_green" />
 
 				<ePixmap position="420,250" size="25,25" pixmap="~/images/key_yellow_sd.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="452,250" zPosition="4" size="280,25" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Virtual keyboard" />
+				<widget position="452,250" zPosition="4" size="280,25" halign="left" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_yellow" />
 				</screen>""" % VER
         else:
         	if isFHD():
@@ -79,13 +92,13 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
 				<widget name="config" position="30,55" size="1020,298" scrollbarMode="showOnDemand" transparent="1" zPosition="2" />
 
 				<ePixmap position="30,360" size="38,38" pixmap="~/images/key_red.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="78,360" zPosition="4" size="300,38" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Cancel" />
+				<widget position="78,360" zPosition="4" size="300,38" halign="left" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_red" />
 
 				<ePixmap position="330,360" size="38,38" pixmap="~/images/key_green.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="378,360" zPosition="4" size="300,38" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Save" />
+				<widget position="378,360" zPosition="4" size="300,38" halign="left" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_green" />
 
 				<ePixmap position="630,360" size="38,38" pixmap="~/images/key_yellow.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="678,360" zPosition="4" size="420,38" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Virtual keyboard" />
+				<widget position="678,360" zPosition="4" size="420,38" halign="left" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_yellow" />
 				</screen>""" % VER
         	else:
         		self.skin = """
@@ -95,13 +108,13 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
 				<widget name="config" position="20,60" size="680,182" scrollbarMode="showOnDemand" transparent="1" zPosition="2" />
 
 				<ePixmap position="20,250" size="25,25" pixmap="~/images/key_red_sd.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="52,250" zPosition="4" size="200,25" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Cancel" />
+				<widget position="52,250" zPosition="4" size="200,25" halign="left" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_red" />
 
 				<ePixmap position="220,250" size="25,25" pixmap="~/images/key_green_sd.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="252,250" zPosition="4" size="200,25" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Save" />
+				<widget position="252,250" zPosition="4" size="200,25" halign="left" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_green" />
 
 				<ePixmap position="420,250" size="25,25" pixmap="~/images/key_yellow_sd.png" zPosition="3" transparent="1" alphatest="blend" />
-				<eLabel position="452,250" zPosition="4" size="280,25" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Virtual keyboard" />
+				<widget position="452,250" zPosition="4" size="280,25" halign="left" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" name="key_yellow" />
 				</screen>""" % VER
         Screen.__init__(self, session)
         self.list = []
@@ -116,16 +129,22 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
         self.fromkeyboard = fromkeyboard
         self['config'] = MenuList([])
         ConfigListScreen.__init__(self, self.list, session=session, on_change=self.changedEntry)
-        self['setupActions'] = ActionMap(['SetupActions', 'ColorActions'], {
-            'green': self.keySave,
-            'yellow': self.showNewkeyboard,
-            'cancel': self.keyClose
-            #"left": self.keyLeft,
-            #"right": self.keyRight
+        self["setupActions"] = ActionMap(["SetupActions", "ColorActions", "EPGSelectActions"],
+        {
+        	"cancel": self.keyClose,
+        	"green": self.keySave,
+        	"yellow": self.showNewkeyboard,
+        	#"left": self.keyLeft,
+        	#"right": self.keyRight
         }, -2)
+        self["key_red"] = Label(_("%s") % title4)
+        self["key_green"] = Label(_("%s") % title5)
+        self["key_yellow"] = Label(_("%s") % title6)
+        self.showinplugins = config.NewVirtualKeyBoard.showinplugins.value
         self.fontssize = config.NewVirtualKeyBoard.fontssize.value
         self.currKeyoboard = config.NewVirtualKeyBoard.textinput.value
         self.showKeyoboard = config.NewVirtualKeyBoard.showsuggestion.value
+        self.langvalue = config.NewVirtualKeyBoard.lang.value
         self.createConfigList()
 
     def changedEntry(self):
@@ -142,14 +161,15 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
                 self.checkupdates()
 
         self.list = []
-        self.list.append(getConfigListEntry(_('Text input method-keyboard:'), config.NewVirtualKeyBoard.textinput))
-        self.list.append(getConfigListEntry(_('Increase and Decrease font size (from 01 to 30):'), config.NewVirtualKeyBoard.fontssize))
+        self.list.append(getConfigListEntry(_('%s:' % title7), config.NewVirtualKeyBoard.textinput))
+        self.list.append(getConfigListEntry(_('%s:' % title8), config.NewVirtualKeyBoard.lang))
+        self.list.append(getConfigListEntry(_('%s:' % title9), config.NewVirtualKeyBoard.fontssize))
         if config.NewVirtualKeyBoard.textinput.value == 'NewVirtualKeyBoard' or value is True:
-            self.list.append(getConfigListEntry(_('Show google and history suggestion:'), config.NewVirtualKeyBoard.showsuggestion))
+            self.list.append(getConfigListEntry(_('%s:' % title10), config.NewVirtualKeyBoard.showsuggestion))
         else:
             pass
-        self.list.append(getConfigListEntry(_('Enable/Disable Checking Online Update:'), config.NewVirtualKeyBoard.updateonline))
-        self.list.append(getConfigListEntry(_('Show plugin in Plugin Browser (Need restart E2):'), config.NewVirtualKeyBoard.showinplugins))
+        self.list.append(getConfigListEntry(_('%s:' % title11), config.NewVirtualKeyBoard.updateonline))
+        self.list.append(getConfigListEntry(_('%s:' % title12), config.NewVirtualKeyBoard.showinplugins))
         self['config'].list = self.list
         self['config'].l.setList(self.list)
 
@@ -167,7 +187,9 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
         pyc_backup = ("/usr/lib/enigma2/python/Screens/VirtualKeyBoard_backup.pyc")
         py_NewVirtualKeyBoard = resolveFilename(SCOPE_PLUGINS, "SystemPlugins/NewVirtualKeyBoard/VirtualKeyBoard.py")
 
-        if not config.NewVirtualKeyBoard.textinput.value == self.currKeyoboard or config.NewVirtualKeyBoard.showsuggestion.value == self.showKeyoboard or not config.NewVirtualKeyBoard.fontssize.value == self.fontssize:
+        if self.currKeyoboard != config.NewVirtualKeyBoard.textinput.value or self.showKeyoboard != config.NewVirtualKeyBoard.showsuggestion.value or \
+        	self.fontssize != config.NewVirtualKeyBoard.fontssize.value or self.showinplugins != config.NewVirtualKeyBoard.showinplugins.value or \
+        	self.langvalue != config.NewVirtualKeyBoard.lang.value:
             if config.NewVirtualKeyBoard.textinput.value == "NewVirtualKeyBoard":
                 #if os.path.exists(pyc_link):
                 #    os.remove(pyc_link)
@@ -183,7 +205,6 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
                     elif os.path.exists(pyc_image):
                         os.rename(pyc_image, pyc_backup)
                     os.symlink(py_NewVirtualKeyBoard, py_link)
-
             elif config.NewVirtualKeyBoard.textinput.value == "VirtualKeyBoard":
                 if os.path.islink(py_link):
                     if os.path.exists(py_backup):
@@ -197,8 +218,7 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
                         os.rename(pyc_backup, pyc_image)
                 else:
                     pass
-
-            self.session.openWithCallback(self.restartenigma, MessageBox, _('Restart enigma2 to load new settings?'), MessageBox.TYPE_YESNO)
+            self.session.openWithCallback(self.restartenigma, MessageBox, _('%s:' % title13), MessageBox.TYPE_YESNO)
         else:
             self.close(True)
 
@@ -251,7 +271,7 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
                 logdata("Updates","No new version available")
         else :
                 new_version = self.new_version
-                self.session.openWithCallback(self.install, MessageBox, _('New version %s is available.\n\nDo want ot install now.' % new_version), MessageBox.TYPE_YESNO)
+                self.session.openWithCallback(self.install, MessageBox, _('%s %s %s.\n\n%s.' % (title14, new_version, title15, title16)), MessageBox.TYPE_YESNO)
 
     def install(self,answer=False):
         try:
@@ -260,7 +280,7 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
                         #cmd="wget http://tunisia-dreambox.info/TSplugins/NewVirtualKeyBoard/installer.sh -O - | /bin/sh"
                         cmd="wget https://raw.githubusercontent.com/fairbird/NewVirtualKeyBoard/main/installer.sh -O - | /bin/sh"
                         cmdlist.append(cmd)
-                        self.session.open(Console, title='Installing last update, enigma will be started after install', cmdlist=cmdlist, finishedCallback=self.myCallback, closeOnSuccess=False)
+                        self.session.open(Console, title='%s' % title17, cmdlist=cmdlist, finishedCallback=self.myCallback, closeOnSuccess=False)
         except:
                 trace_error()
         
