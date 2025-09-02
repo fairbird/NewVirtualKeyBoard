1fc45b7f5653bce4a36cef984f59884d032c2d23
RAED <rrrr53@hotmail.com>
2025-09-02 18:36:59 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##setup command=wget https://raw.githubusercontent.com/fairbird/NewVirtualKeyBoard/main/installer.sh -O - | /bin/sh
 ###########
-version="13.4"
+version="13.5"
 description="
 What is NEW :
 - Some fix codes
